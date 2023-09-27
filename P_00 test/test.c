68928500fc0216f99c3a29c2ac3cd16169b19ac7
Passer1072 <csy108729@gmail.com>
2023-09-28 01:01:20 +0800

    加入了选择排序
    加入了选择选择排序（升序），优化了删除学号的代码。

--- P_00 test/test.c
+++ P_00 test/test.c
@@ -1,15 +1,15 @@
 //#include <stdio.h>
 
-   // ���庯�������﷨
-//�������� ��������(�����б�) {
-//    // �����壬��������Ĵ���ʵ��
-//    // ���԰����ֲ����������
-//    return ����ֵ; // ����ֵ���ͱ����뺯�������ķ�������ƥ��
+   // 定义函数基本语法
+//返回类型 函数名称(参数列表) {
+//    // 函数体，包含具体的代码实现
+//    // 可以包含局部变量和语句
+//    return 返回值; // 返回值类型必须与函数声明的返回类型匹配
 //}
 
 
 
-// �Զ��庯�������ڼ������������ĺ�
+// 自定义函数，用于计算两个整数的和
 //int chaXun() {
 //    int result = a + b;
 //    return chaXun();
@@ -19,192 +19,192 @@
 //    int num1 = 5;
 //    int num2 = 3;
 //
-//    // �����Զ��庯����������洢�ڱ���sum��
+//    // 调用自定义函数并将结果存储在变量sum中
 //    int sum = add(num1, num2);
 //
-//    // ��ӡ������
-//    printf("�ͣ�%d\n", sum);
+//    // 打印计算结果
+//    printf("和：%d\n", sum);
 //
 //    return 0;
 //}
 
 
 #include <stdio.h>
-// ȫ�ֱ���
+// 全局变量
 int choice;
 int const MaxStu = 5;
-int exitFlag = 0; // �����Ƿ��˳��ı�־����ʼΪ0��ʾ���˳�
-int passer[5][2]; // ���⣺�����ά�ȱ����ǳ�������ʽ
+int exitFlag = 0; // 控制是否退出的标志，初始为0表示不退出
+int passer[5][2]; // 特殊：数组的维度必须是常量表达式
 
 void xuanxiang(){
-    // ��ӡѡ��
-    printf("   ��  ��\n");
-    printf("1.ѧ���ɼ�¼��\n");
-    printf("2.ѧ���ɼ��޸�\n");
-    printf("3.ѧ���ɼ�ɾ��\n");
-    printf("4.ѧ���ɼ���ѯ\n");
-    printf("5.ѧ���ɼ�����\n");
-    printf("6.ѧ���ɼ�����(����)\n");
-    printf("0.��       ��\n");
-    printf("��������ţ�");
-    // scanf �� C ���������ڴ��û����ļ���ȡ����ĺ�����������˼��ɨ���ʽ���ַ�����
+    // 打印选项
+    printf("   菜  单\n");
+    printf("1.学生成绩录入\n");
+    printf("2.学生成绩修改\n");
+    printf("3.学生成绩删除\n");
+    printf("4.学生成绩查询\n");
+    printf("5.学生成绩保存\n");
+    printf("6.学生成绩排序(升序)\n");
+    printf("0.退       出\n");
+    printf("请输入序号：");
+    // scanf 是 C 语言中用于从用户或文件读取输入的函数，它的意思是扫描格式化字符串。
     scanf("%d", &choice);
-}           // ���庯��xuanxiang��ӡȫ��ѡ��������û��������Ŀ���
+}           // 定义函数xuanxiang打印全部选项，并保存用户输入的项目编号
 void luRu(){
-    // ¼��ɼ�
+    // 录入成绩
     for (
             int i = 0; i < MaxStu ; ++i) {
-        //�Զ�д���к�
+        //自动写入行号
         passer[i][0] = i + 1;
-        //�ֶ�����ڶ��гɼ�
-        printf("������%dѧ���ɼ���", i + 1);
+        //手动输入第二列成绩
+        printf("请输入%d学生成绩：", i + 1);
         scanf("%d", &passer[i][1]);
     }
 
-    // ��ӡ��ά����
+    // 打印二维数组
     for (int i = 0; i < MaxStu ; ++i) {
-        // ��ӡ��0��ѧ��
+        // 打印第0列学号
         printf("%-5d", passer[i][0]);
-        // ��ӡ��1�гɼ�
-        printf("%d��\n", passer[i][1]);
+        // 打印第1列成绩
+        printf("%d分\n", passer[i][1]);
     }
-}                // ���庯��luRu���гɼ�¼�롣
+}                // 定义函数luRu进行成绩录入。
 void xiuGai(){
     int StudentID;
-    printf("����ѧ�ţ�");
+    printf("输入学号：");
     scanf("%d", &StudentID);
 
-    int found = 0; // ���ڱ���Ƿ��ҵ�ѧ��
+    int found = 0; // 用于标记是否找到学生
 
     for (int i = 0; i < 5; ++i) {
         if (passer[i][0] == StudentID) {
-            printf("�������µĳɼ���");
+            printf("请输入新的成绩：");
             scanf("%d", &passer[i][1]);
-            found = 1; // ���Ϊ�ҵ�ѧ��
-            break; // �ҵ�ѧ�����˳�ѭ��
+            found = 1; // 标记为找到学生
+            break; // 找到学生后退出循环
         }
     }
 
     if (!found) {
-        printf("δ�ҵ���ѧ��\n");
+        printf("未找到该学生\n");
     }
-}              // ���庯��xiuGai���гɼ��޸�
+}              // 定义函数xiuGai进行成绩修改
 void DeleteStuGrades(){
     int StuNumber;
     int Found = 0;
-    printf("������Ҫɾ����ѧ��:");
-    // ��ȡѧ��
+    printf("请输入要删除的学号:");
+    // 读取学号
     scanf("%d",&StuNumber);
-    // ����Ҫɾ����ѧ��ѧ������
-    // ��0�п�ʼ������λ����
+    // 查找要删除的学生学号索引
+    // 从0行开始遍历二位函数
     for (int i = 0; i < MaxStu; ++i) {
-        // �ж��Ƿ��ҵ�
+        // 判断是否找到
         if(passer[i][0] == StuNumber){
-            // ����ֵ�����ҵ�ѧ��
+            // 返回值，已找到学生
             Found = 1;
-            printf("���ҵ�ѧ��,��ʼɾ��\n");
-            // ��Ҫɾ����λ�ÿ�ʼ��������ѧ��������������
+            printf("已找到学生,开始删除\n");
+            // 从要删除的位置开始，将后续学生数据依次上移
             for (int j = i; j < MaxStu - 1; ++j) {
                 passer[j][0] = passer[j + 1][0];
                 passer[j][1] = passer[j + 1][1];
 
             }
-            // ������һ�е�����
+            // 清除最后一行的数据
             passer[MaxStu - 1][0] = 0;
             passer[MaxStu - 1][1] = 0;
-            printf("ɾ�����\n");
+            printf("删除完成\n");
 
-            // ���Ż���ɾ����ɺ�ֱ��break����˳�ѭ��
+            // （优化）删除完成后直接break打断退出循环
             break;
         }
     }
-    // ��ٺ����ж�
+    // 真假函数判断
     if(!Found){
-        printf("δ�ҵ���ѧ��\n");
+        printf("未找到该学生\n");
     }
-}     // ��һ��BUG-�ѽ��������������DeleteStuGrades���гɼ�ɾ��
+}     // 法一（BUG-已解决）：定音函数DeleteStuGrades进行成绩删除
 void chaXun(){
 int X;
 int exitFlag = 0;
-    // while (!exitFlag)Ϊ�棨1��ʱ����ѭ����Ϊ�٣�0��ʱ����ѭ����
+    // while (!exitFlag)为真（1）时结束循环，为假（0）时继续循环。
     while (!exitFlag) {
-        printf("����Ҫ��ѯ�ı�ţ�0�˳�����");
-        // �Ӽ�������洢��X�С�
+        printf("输入要查询的编号（0退出）：");
+        // 从键盘输入存储在X中。
         scanf("%d", &X );
-        // ==�����ж��������ߣ��ж�ѡ��X�Ƿ�Ϊ0.
+        // ==用于判断左右两边，判断选项X是否为0.
         if (X == 0){
-            // �˳�ѭ���ı��
+            // 退出循环的编号
             exitFlag = 1;
         } else {
-            // ����Ƿ��ҵ�ѧ����ţ�Ĭ�ϱ��0����û���ҵ���
-            // ��ϸ�߼����ͣ����ȳ�ʼ��FoundΪ�٣�Ȼ��ʼforѭ����ͨ��if�ж���Ѱ����Xֵ��ȵĶ�ά����һ�е�ѧ�ţ�1.����ҵ���ִ��if�еĺ�������ӡ
-            //����Ӧ��ѧ����ɼ�������Found���Ϊ1���棩��ʾ�Ѿ��ҵ����������forѭ����2.���û���ҵ��򲻻�ִ��if�еĺ��������forѭ����Ȼû���ҵ����ֵ֮��
-            //Found��Ȼ����0(��)���Զ�����forѭ������if(!Found)���жϣ�Ϊ�棨1��ʱ���У�Ϊ�٣�0�������������������У�������ȡ������������Found = 0�����û���ҵ���
-            //ʱִ��if�к��������δ�ҵ���ѧ������
+            // 标记是否找到学生编号，默认标记0代表没有找到。
+            // 详细逻辑解释：首先初始化Found为假，然后开始for循环，通过if判断来寻找与X值相等的二维表第一列的学号，1.如果找到则执行if中的函数，打印
+            //出对应的学号与成绩，并将Found标记为1（真）表示已经找到，随后跳出for循环。2.如果没有找到则不会执行if中的函数，五次for循环依然没有找到相等值之后
+            //Found依然保持0(假)，自动跳出for循环经过if(!Found)的判断，为真（1）时运行，为假（0）则跳过，但是这里有！号所以取反，所以这里Found = 0（标记没有找到）
+            //时执行if中函数输出“未找到该学生”。
             int Found = 0;
-            // ѭ�����Ҷ�Ӧ��ѧ��
+            // 循环查找对应的学号
             for (int i = 0; i < 5; ++i) {
                 if(passer[i][0] == X){
-                    // �ҵ����ӡ��Ӧѧ����ɼ�
-                    printf("ѧ�ţ�%d  �ɼ���%d\n",X,passer[i][1]);
-                    // �ҵ�����Found = 1��ʾ���ҵ�
+                    // 找到后打印对应学号与成绩
+                    printf("学号：%d  成绩：%d\n",X,passer[i][1]);
+                    // 找到后标记Found = 1表示已找到
                     Found = 1;
                     break;
                 }
             }
             if(!Found){
-                printf("δ�ҵ���ѧ��\n");
+                printf("未找到该学生\n");
             }
         }
     }
-}              // ���庯��chaXun���гɼ���ѯ
+}              // 定义函数chaXun进行成绩查询
 void SaveGradesToFile() {
-    // ����һ�������Ķ��壬��ʾfile��һ��ָ��FILE���͵�ָ�룬FILE������C���������ڱ�ʾ�ļ��Ľṹ�塣
+    // 这是一个变量的定义，表示file是一个指向FILE类型的指针，FILE类型是C语言中用于表示文件的结构体。
     FILE *file;
-    // ��fopen������һ����Ϊgrades.txt���ļ�������д��ģʽ����w������
-    file = fopen("grades.txt", "w"); // ��һ���ļ���д�����ݣ�����ļ������ڣ���ᴴ���ļ���
+    // 用fopen函数打开一个名为grades.txt的文件，并以写入模式（“w”）打开
+    file = fopen("grades.txt", "w"); // 打开一个文件来写入数据（如果文件不存在，则会创建文件）
 
-    // ���file��������NULL��˵���ļ���ʧ�ܣ���ִ�д������������䡣
+    // 如果file变量等于NULL，说明文件打开失败，就执行大括号里面的语句。
     if (file == NULL) {
-        printf("�޷����ļ�\n");
+        printf("无法打开文件\n");
         return;
     }
 
-    // ��ѧ���ɼ�����д���ļ�
+    // 将学生成绩数据写入文件
     for (int i = 0; i < MaxStu; ++i) {
-        // ����һ�������䣬��ʾ��passer[i][0]��passeri������������������ʾ��iλѧ����ѧ�źͳɼ�������"%d %d\n"�ĸ�ʽ����������֮���пո�
-        //�����л��з���д�뵽fileָ����ļ��С�passer��һ����ά���飬���ڴ洢����ѧ���ĳɼ����ݡ�
+        // 这是一个输出语句，表示将passer[i][0]和passeri（两个整数变量，表示第i位学生的学号和成绩）按照"%d %d\n"的格式（两个整数之间有空格，
+        //后面有换行符）写入到file指向的文件中。passer是一个二维数组，用于存储所有学生的成绩数据。
         fprintf(file, "%d %d\n", passer[i][0], passer[i][1]);
     }
 
-    fclose(file); // �ر��ļ�
-    printf("ѧ���ɼ��ѱ��浽�ļ� grades.txt\n");
-}   // ���庯��SaveGradesToFile���浱ǰ��ά���ڵĳɼ���grades.txt
+    fclose(file); // 关闭文件
+    printf("学生成绩已保存到文件 grades.txt\n");
+}   // 定义函数SaveGradesToFile保存当前二维表内的成绩至grades.txt
 void choicePaiXu(){
-    int n = 5;  // ��ʾѧ���ɼ��� passer �е�������ѧ����������
-    int temp0;  // temp :������Ԫ�ؽ���ʱ��ʱ�洢Ԫ�ص�ֵ��
+    int n = 5;  // 表示学生成绩表 passer 中的行数或学生的数量。
+    int temp0;  // temp :用于在元素交换时暂时存储元素的值。
     int temp1;
     /*
-     ��һ�ж����˼�������������������ѡ�������е�ѭ����Ԫ�ؽ�����ʹ�á�
-     i �� j :��ѭ��������
-     minIndex :���ڴ洢δ���򲿷ֵ���СԪ�ص�������
+     这一行定义了几个整数变量，用于在选择排序中的循环和元素交换中使用。
+     i 和 j :是循环计数器
+     minIndex :用于存储未排序部分的最小元素的索引，
      */
     int i, j, minIndex;
 
-    // ��ӡ��ʾ�δ��������顣
-    printf("δ��������飺\n");
+    // 打印提示语：未排序的数组。
+    printf("未排序的数组：\n");
     for (int i = 0; i < n; i++) {
         printf("%d ", passer[i][0]);
         printf("%d\n", passer[i][1]);
     }
 
-// ������������������������������������������������������������������������������������������������������������������������������������������������������������������������������������������������
+// ————————————————————————————————————————————————————————————————————————————————————————————————
     for (i = 0; i < n - 1; i++) {
-        // ���赱ǰѭ����ʼʱ����һ��Ԫ������С�ģ��ӵ�һ������ʼ������ѯ�Ա�
+        // 假设当前循环开始时，第一个元素是最小的，从第一个数开始遍历查询对比
         minIndex = i;
-        // ��δ���򲿷��ҵ���СԪ�ص�����
+        // 在未排序部分找到最小元素的索引
         /*
-         ���chengJi[j]�е���С��chengJi[minIndex]�е�������minIndex��Ϊj��
+         如果chengJi[j]中的数小于chengJi[minIndex]中的数，则将minIndex变为j。
          */
         for (j = i + 1; j < n; j++) {
             if (passer[j][1] < passer[minIndex][1]) {
@@ -212,117 +212,117 @@ void choicePaiXu(){
             }
         }
 
-        // ����СԪ���뵱ǰλ�õ�Ԫ�ؽ���
-        temp0 = passer[i][0];// 1.����ǰԪ�ص�ֵ�洢�� temp ��
+        // 将最小元素与当前位置的元素交换
+        temp0 = passer[i][0];// 1.将当前元素的值存储在 temp 中
         temp1 = passer[i][1];
-        passer[i][0] = passer[minIndex][0];  // 2.����СԪ�ص�ֵ���Ƶ���ǰλ��
+        passer[i][0] = passer[minIndex][0];  // 2.将最小元素的值复制到当前位置
         passer[i][1] = passer[minIndex][1];
-        passer[minIndex][0] = temp0;    // 3.�� temp �е�ֵ������СԪ�ص�λ�á�
+        passer[minIndex][0] = temp0;    // 3.将 temp 中的值赋给最小元素的位置。
         passer[minIndex][1] = temp1;
     }
-// ������������������������������������������������������������������������������������������������������������������������������������������������������������������������������������������������
-    // ��ӡ��ʾ����������顣
-    printf("\n���������飺\n");
+// ————————————————————————————————————————————————————————————————————————————————————————————————
+    // 打印提示语：排序后的数组。
+    printf("\n排序后的数组：\n");
     for (int i = 0; i < n; i++) {
         printf("%d ", passer[i][0]);
         printf("%d\n", passer[i][1]);
     }
-}         // ѡ������
+}         // 选择排序
 void DeleteStuGrades2() {
     int StuNumber;
-    printf("������Ҫɾ����ѧ��:");
+    printf("请输入要删除的学号:");
     scanf("%d", &StuNumber);
 
-    int found = 0; // ���ڱ���Ƿ��ҵ�ѧ��
+    int found = 0; // 用于标记是否找到学生
 
     for (int i = 0; i < MaxStu; ++i) {
         if (passer[i][0] == StuNumber) {
-            found = 1; // ���Ϊ�ҵ�ѧ��
-            // ɾ��ѧ��ΪStuNumber��ѧ����������ѧ��������������һ��λ��
+            found = 1; // 标记为找到学生
+            // 删除学号为StuNumber的学生，将后续学生数据依次上移一个位置
             for (int j = i; j < MaxStu - 1; ++j) {
                 passer[j][0] = passer[j + 1][0];
                 passer[j][1] = passer[j + 1][1];
             }
-            // ������һ�е�����
+            // 清除最后一行的数据
             passer[MaxStu - 1][0] = 0;
             passer[MaxStu - 1][1] = 0;
-            printf("ɾ�����\n");
+            printf("删除完成\n");
             break;
         }
     }
 
     if (!found) {
-        printf("δ�ҵ���ѧ��\n");
+        printf("未找到该学生\n");
     }
-}   // ��������������DeleteStuGrades���гɼ�ɾ��
+}   // 法二：定音函数DeleteStuGrades进行成绩删除
 void ChoiceByIf(){
     int X;
     int Y;
-    // ��ifʵ����Ŀѡ��
+    // 用if实现项目选择
         if(choice >= 0 && choice <= 5 ) {
-            // 1.ѧ���ɼ�¼��
+            // 1.学生成绩录入
             if (choice == 1) {
-                printf("������ѧ���ɼ�\n");
-                // ¼��ɼ�
+                printf("请输入学生成绩\n");
+                // 录入成绩
                 for (int i = 0; i < MaxStu ; ++i) {
-                    //�Զ�д���к�
+                    //自动写入行号
                     passer[i][0] = i + 1;
-                    //�ֶ�����ڶ��гɼ�
-                    printf("������%dѧ���ɼ���", i + 1);
+                    //手动输入第二列成绩
+                    printf("请输入%d学生成绩：", i + 1);
                     scanf("%d", &passer[i][1]);
                 }
-                // ��ӡ��ά����
+                // 打印二维数组
                 for (int i = 0; i < MaxStu ; ++i) {
-                    // ��ӡ��0��ѧ��
+                    // 打印第0列学号
                     printf("%-5d", passer[i][0]);
-                    // ��ӡ��1�гɼ�
-                    printf("%d��\n", passer[i][1]);
+                    // 打印第1列成绩
+                    printf("%d分\n", passer[i][1]);
                 }
             }
 
             if (choice == 2) {
-                // 2.ѧ���ɼ��޸�
-                printf("ѧ���ɼ��޸�\n");
+                // 2.学生成绩修改
+                printf("学生成绩修改\n");
             }
             if (choice == 3) {
-                // 3.ѧ���ɼ�ɾ��
-                printf("ѧ���ɼ�ɾ��\n");
+                // 3.学生成绩删除
+                printf("学生成绩删除\n");
             }
             if (choice == 4) {
-                // 4.ѧ���ɼ���ѯ
-                printf("ѧ���ɼ���ѯ\n");
-            // ��ѯ���ѭ������!exitFlagΪ��ʱ������Ϊ��ʱ�˳���
+                // 4.学生成绩查询
+                printf("学生成绩查询\n");
+            // 查询命令（循环），!exitFlag为真时继续，为假时退出。
                 while (!exitFlag) {
-                    printf("����Ҫ��ѯ�ı�ţ�");
-                    // �Ӽ�������洢��X�С�
+                    printf("输入要查询的编号：");
+                    // 从键盘输入存储在X中。
                     scanf("%d", &X );
-                    // ==�����ж���������
+                    // ==用于判断左右两边
                     if (X == 0){
                         exitFlag = 1;
                     } else {
                         Y = X - 1;
-                        // �ж�ѧ���Ƿ���1-5��������
+                        // 判断学号是否在1-5的区间中
                         if(Y >= 0 && Y < MaxStu) {
-                            // ��ӡ�����ж�Ӧֵ������
-                            printf("���ĳɼ���%d\n", passer[Y][1]);
+                            // 打印表格中对应值并换行
+                            printf("您的成绩：%d\n", passer[Y][1]);
                         }else{
-                            printf("��Чѧ�ţ�����������\n");
+                            printf("无效学号，请重新输入\n");
                         }
                     }
                 }
             }
             if (choice == 5) {
-                printf("ѧ���ɼ�����\n");
+                printf("学生成绩保存\n");
             }
             if (choice == X) {
-                printf("�˳�����");
+                printf("退出程序");
             }
         } else{
-            printf("ѡ����Ч������������\n");
+            printf("选择无效，请重新输入\n");
         }
-}          // ���庯��ChoiceByIf����Ŀѡ��һ��if
+}          // 定义函数ChoiceByIf，项目选择法一：if
 void ChoiceByIfelse(){
-     // ʹ��if...elseʵ����Ŀѡ��
+     // 使用if...else实现项目选择
         if (choice == 1){
             printf("1\n");
         } else if (choice == 2){
@@ -334,84 +334,84 @@ void ChoiceByIfelse(){
         } else if (choice == 5){
             printf("5\n");
         } else if (choice == 0){
-            printf(" ��������\n");
+            printf(" 结束程序\n");
 
         } else {
-            printf("��Ч\n") ;
+            printf("无效\n") ;
         }
-}      // ���庯��ChoiceByIfelse����Ŀѡ�񷨶���if...else
+}      // 定义函数ChoiceByIfelse，项目选择法二：if...else
 void testAll(){
-    // ��ӡ��ά����
+    // 打印二维数组
     for (int i = 0; i < MaxStu ; ++i) {
-        // ��ӡ��0��ѧ��
+        // 打印第0列学号
         printf("%-5d", passer[i][0]);
-        // ��ӡ��1�гɼ�
-        printf("%d��\n", passer[i][1]);
+        // 打印第1列成绩
+        printf("%d分\n", passer[i][1]);
     }
-}             // test����ӡ��ά����
+}             // test：打印二维数组
 
 
 int main(){
 
     do {
-        // ����ѡ�����xuanxiang��
+        // 调用选项含函数xuanxiang。
         xuanxiang();
 
-        // ʹ��switchʵ����Ŀѡ��
-        // switch����:���� choice ������ֵ��ѡ��ִ����һ�� case ��֧��
+        // 使用switch实现项目选择
+        // switch解释:根据 choice 变量的值来选择执行哪一个 case 分支。
         switch (choice) {
             case 1:
-                printf("1.ѧ���ɼ�¼��\n");
-               luRu();// ���ú���luRu����¼��ɼ�
+                printf("1.学生成绩录入\n");
+               luRu();// 调用函数luRu用于录入成绩
 
-                break;// break����������� switch ��䣬��ִֹ������ case ����䡣
+                break;// break语句用于跳出 switch 语句，防止执行其他 case 的语句。
             case 2:
-                printf("2.ѧ���ɼ��޸�\n");
-                xiuGai();// ���ú���xiuGai�����޸ĳɼ�
+                printf("2.学生成绩修改\n");
+                xiuGai();// 调用函数xiuGai用于修改成绩
 
                 break;
             case 3:
-                printf("3.ѧ���ɼ�ɾ��\n");
-                DeleteStuGrades();// ����DeleteStuGrades����ɾ��ѧ���ɼ�
+                printf("3.学生成绩删除\n");
+                DeleteStuGrades();// 调用DeleteStuGrades用于删除学生成绩
                 break;
             case 4:
-                printf("4.ѧ���ɼ���ѯ\n");
-               chaXun();// ���ò�ѯ����chaXun���ڲ�ѯ�ɼ���ѭ����
+                printf("4.学生成绩查询\n");
+               chaXun();// 调用查询函数chaXun用于查询成绩（循环）
 
                 break;
             case 5:
-                printf("5.ѧ���ɼ�����\n");
-                SaveGradesToFile();// ����SaveGradesToFile��������ɼ���grades.txt
+                printf("5.学生成绩保存\n");
+                SaveGradesToFile();// 调用SaveGradesToFile函数保存成绩至grades.txt
                 break;
 
             case 6:
-                printf("7.ѡ������\n");
+                printf("7.选择排序\n");
                 choicePaiXu();
                 break;
 
             case 999:
-                printf("test����ӡĿǰ��ά����\n");
-                testAll();// ���ԣ�������ӡ��ǰ����
+                printf("test：打印目前二维数组\n");
+                testAll();// 测试：遍历打印当前数组
                 break;
 
 
             case 0:
-                printf("�˳�\n");
-                // ����ѭ���˳�����
+                printf("退出\n");
+                // 跳出循环退出程序
                 break;
 
             default:
-                printf("��Ч��ѡ��������ѡ��\n");
+                printf("无效的选择，请重新选择\n");
         }
         printf("\n");
 
 
-        // �ȴ��û��������ⰴ��
-        printf("���»س�������...\n");
+        // 等待用户按下任意按键
+        printf("按下回车键继续...\n");
         getchar();
-        getchar(); // ���� getchar ���ڵȴ��û����»س���
-        // ѭ�����������̾�� ! ���߼������������ʾ�߼��ǣ�logical NOT��������������ȡ����0��1�����桰�䡰�١���
-        // ��choice������0ʱ��ѭ������ִ�У�����ѭ��ֹͣ��Ϊ�棨1��ʱ����ѭ����Ϊ�٣�0��ʱֹͣѭ����
+        getchar(); // 两次 getchar 用于等待用户按下回车键
+        // 循环体结束；感叹号 ! 是逻辑运算符，它表示逻辑非（logical NOT）。它的作用是取反，0变1，”真“变“假”。
+        // 当choice不等于0时，循环继续执行，否则循环停止。为真（1）时继续循环，为假（0）时停止循环。
     }while (choice !=0);
 
     return 0;
